2ea72afff54a57122dbb73b78484d3b51a5aedb4
lxl <luxiaolitiancai@qq.com>
2016-03-22 16:46:50 +0800

    3-22 初步建好Main的界面

---
+++ app/static/for_List.js
@@ -0,0 +1,7 @@
+function myhostpital(){
+	//alert("hello");
+	var edf_files = document.getElementById("list_edf_files");
+	if (edf_files.style.display == "none")
+		edf_files.style.display = "block";
+	else edf_files.style.display = "none";
+}
