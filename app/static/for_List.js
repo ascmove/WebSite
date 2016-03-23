c2284df68484626606c1a80ff0af825070243c54
lxl <luxiaolitiancai@qq.com>
2016-03-23 10:46:12 +0800

    给主页面加上右上角的导航栏与翻页

--- app/static/for_List.js
+++ app/static/for_List.js
@@ -1,7 +1,9 @@
 function myhostpital(){
 	//alert("hello");
-	var edf_files = document.getElementById("list_edf_files");
-	if (edf_files.style.display == "none")
-		edf_files.style.display = "block";
-	else edf_files.style.display = "none";
+	var edf_files = document.getElementsByClassName("list_edf_files");
+	for(e=0;e<edf_files.length;e++){
+		if (edf_files[e].style.display == "none")
+			edf_files[e].style.display = "block";
+		else edf_files[e].style.display = "none";
+	}
 }
